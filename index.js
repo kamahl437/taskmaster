511e1dfbe18f3839912d3fa9f4fb609bbda53265
JQ <kamahl437@gmail.com>
2018-10-26 21:33:33 -0400

    chaging to query by name

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ router.post('/user/:userId/task/:taskId', function(req, res, next) {
     let taskId = req.params.taskId;
     getTaskQueueCollection()
     .then((taskQueues) => {
-        taskQueues.find({_id:userId})
+        taskQueues.find({name:userId})
             .toArray()
             .then((docs) => {
                 let user = docs[0];
